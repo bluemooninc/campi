035b36e26c8148db23c89d7c7c164f0b23b318bc
root <root@raspberrypi.(none)>
2016-04-02 02:11:56 +0900

    support m3u8

--- opencv/dailymp4.py
+++ opencv/dailymp4.py
@@ -5,7 +5,7 @@ import time
 import datetime
 import os
 import glob
-
+import subprocess as sp
 
 inifile = ConfigParser.SafeConfigParser()
 inifile.read("/home/pi/camlaps.ini")
@@ -14,8 +14,24 @@ delay = inifile.getint("camera","delay")
 upfolder = inifile.get("user","uploadFolder")
 
 dt = datetime.datetime.today()
-fname = "weekday%d.mp4" % (dt.weekday(),)
-UPLOAD_PATH = upfolder + serialno + '/' + fname +'.jpg'
+mp4fname = "weekday%d.mp4" % (dt.weekday(),)
+UPLOAD_PATH = upfolder + serialno + '/' + mp4fname +'.jpg'
 newest = max(glob.iglob('/tmp/img*.jpg'), key=os.path.getctime)
 print newest
 scp.upload(newest,UPLOAD_PATH)
+
+## ffmpeg make m3u8 and upload
+fname, ext = os.path.splitext(mp4fname)
+command = 'ffmpeg -i /tmp/' + mp4fname + \
+    ' -codec copy -map 0 -f segment -vbsf h264_mp4toannexb' + \
+    ' -segment_format mpegts -segment_time 10' + \
+    ' -segment_list /tmp/' + fname + '-playlist.m3u8' + \
+    ' /tmp/' + fname + '-%03d.ts'
+print command
+ret  =  sp.check_output( command.split(" ") )
+print ret
+
+for name in glob.glob('/tmp/' + fname + '-*'):
+    print name
+    scp.upload(name,UPLOAD_PATH)
+
