8f21b14050be3e8dc70220452fffcbd84331b424
root <root@raspberrypi.(none)>
2016-05-04 07:09:29 +0900

    m3u8 to mp4

--- opencv/dailymp4.py
+++ opencv/dailymp4.py
@@ -21,6 +21,7 @@ dt = datetime.datetime.today()
 mp4fname = "weekday%d.mp4" % (dt.weekday(),)
 fname, ext = os.path.splitext(mp4fname)
 UPLOAD_PATH = upfolder + serialno + '/'
+scp.upload(mp4fname,UPLOAD_PATH + fname + ext)
 newest = max(glob.iglob('/home/pi/picture/img*.jpg'), key=os.path.getctime)
 print newest
 shutil.copyfile(newest,'/home/pi/picture/'+fname+'.jpg')
@@ -28,15 +29,15 @@ scp.upload(newest,UPLOAD_PATH + fname +'.jpg')
 ##
 ## ffmpeg make m3u8 and upload
 ##
-command = 'ffmpeg -i /home/pi/picture/' + mp4fname + \
-    ' -codec copy -map 0 -f segment -vbsf h264_mp4toannexb' + \
-    ' -segment_format mpegts -segment_time 10' + \
-    ' -segment_list /home/pi/picture/' + fname + '-playlist.m3u8' + \
-    ' /home/pi/picture/' + fname + '-%03d.ts'
-print command
-ret  =  sp.check_output( command.split(" ") )
-print ret
-for name in glob.glob('/home/pi/picture/' + fname + '-*'):
-    print name
-    scp.upload(name,UPLOAD_PATH + os.path.basename(name))
+##command = 'ffmpeg -i /home/pi/picture/' + mp4fname + \
+##    ' -codec copy -map 0 -f segment -vbsf h264_mp4toannexb' + \
+##    ' -segment_format mpegts -segment_time 10' + \
+##    ' -segment_list /home/pi/picture/' + fname + '-playlist.m3u8' + \
+##    ' /home/pi/picture/' + fname + '-%03d.ts'
+##print command
+##ret  =  sp.check_output( command.split(" ") )
+##print ret
+##for name in glob.glob('/home/pi/picture/' + fname + '-*'):
+##    print name
+##    scp.upload(name,UPLOAD_PATH + os.path.basename(name))
 
